975ad3bb28a8a95fda111a464a5a6726948beb85
Eric Lau <ericglau@outlook.com>
2021-11-07 20:25:48 -0500

    Refactor beacon upgrade

--- packages/plugin-hardhat/src/upgrade-beacon.ts
+++ packages/plugin-hardhat/src/upgrade-beacon.ts
@@ -1,56 +1,41 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import type { ethers, ContractFactory, Contract, Signer } from 'ethers';
 
-import { Manifest, getAdminAddress, getCode, getBeaconAddress, ProxyDeployment } from '@openzeppelin/upgrades-core';
+import { ProxyDeployment } from '@openzeppelin/upgrades-core';
 
 import {
-  UpgradeOptions,
-  deployImpl,
-  getTransparentUpgradeableProxyFactory,
-  getProxyAdminFactory,
   getContractAddress,
   ContractAddressOrInstance,
   getUpgradeableBeaconFactory,
   deployImplForBeacon,
+  Options,
 } from './utils';
 
 export type UpgradeBeaconFunction = (
   beacon: ContractAddressOrInstance,
-  ImplFactory: ContractFactory
+  ImplFactory: ContractFactory,
+  opts?: Options,
 ) => Promise<Contract>;
 
 export function makeUpgradeBeacon(hre: HardhatRuntimeEnvironment): UpgradeBeaconFunction {
-  return async function upgradeBeacon(beacon, ImplFactory) {
+  return async function upgradeBeacon(beacon, ImplFactory, opts: Options = {}) {
     const beaconAddress = getContractAddress(beacon);
 
-    const opts = { kind: 'beacon' } as UpgradeOptions; // TODO
     const { impl: nextImpl } = await deployImplForBeacon(hre, ImplFactory, opts, beaconAddress);
-    // upgrade kind is inferred above
-    const upgradeTo = await getBeaconUpgrader(beaconAddress, opts.kind, ImplFactory.signer);
+    const upgradeTo = await getBeaconUpgrader(beaconAddress, ImplFactory.signer);
     const upgradeTx = await upgradeTo(nextImpl);
-
-/*    const { provider } = hre.network;
-    const manifest = await Manifest.forNetwork(provider);
-    await manifest.addBeacon(beaconDeployment);*/
-
-
     
-
     // TODO avoid duplicate code from below
     const UpgradeableBeaconFactory = await getUpgradeableBeaconFactory(hre, ImplFactory.signer);
     const beaconContract = UpgradeableBeaconFactory.attach(beaconAddress);
     // @ts-ignore Won't be readonly because inst was created through attach.
     beaconContract.deployTransaction = upgradeTx;
     return beaconContract;
-
   };
 
   type BeaconUpgrader = (nextImpl: string) => Promise<ethers.providers.TransactionResponse>;
 
-  async function getBeaconUpgrader(beaconAddress: string, kind: ProxyDeployment['kind'] | undefined, signer: Signer): Promise<BeaconUpgrader> {
-    //const { provider } = hre.network;
-
-    //const currentBeaconAddress = await getBeaconAddress(provider, proxyAddress);
+  async function getBeaconUpgrader(beaconAddress: string, signer: Signer): Promise<BeaconUpgrader> {
     const UpgradeableBeaconFactory = await getUpgradeableBeaconFactory(hre, signer);
     const beaconContract = UpgradeableBeaconFactory.attach(beaconAddress);
 

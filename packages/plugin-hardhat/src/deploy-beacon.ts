ffa8a1f4ff42b750512e4818e9e193ae483e2d5f
Eric Lau <ericglau@outlook.com>
2021-11-07 20:18:25 -0500

    Cleaned up deployBeacon

--- packages/plugin-hardhat/src/deploy-beacon.ts
+++ packages/plugin-hardhat/src/deploy-beacon.ts
@@ -1,49 +1,35 @@
 import type { HardhatRuntimeEnvironment } from 'hardhat/types';
 import type { ContractFactory, Contract } from 'ethers';
 
-import { Manifest, fetchOrDeployAdmin, logWarning, BeaconDeployment } from '@openzeppelin/upgrades-core';
+import { Manifest, BeaconDeployment } from '@openzeppelin/upgrades-core';
 
 import {
   DeployOptions,
   deploy,
-  deployImpl,
-  getProxyFactory,
-  getTransparentUpgradeableProxyFactory,
-  getProxyAdminFactory,
   DeployTransaction,
-  getBeaconProxyFactory,
   getUpgradeableBeaconFactory,
+  deployImplForBeacon,
 } from './utils';
 
 export interface DeployBeaconFunction {
-  (ImplFactory: ContractFactory): Promise<Contract>;
+  (ImplFactory: ContractFactory, opts?: DeployOptions): Promise<Contract>;
 }
 
 export function makeDeployBeacon(hre: HardhatRuntimeEnvironment): DeployBeaconFunction {
   return async function deployBeacon(
     ImplFactory: ContractFactory,
+    opts: DeployOptions = {},
   ) {
 
     const { provider } = hre.network;
     const manifest = await Manifest.forNetwork(provider);
 
-    const { impl } = await deployImpl(hre, ImplFactory, { kind: 'beacon' } as DeployOptions);
-    //const data = getInitializerData(ImplFactory, args, opts.initializer);
-
-    if (await manifest.getAdmin()) {
-      logWarning(`A proxy admin was previously deployed on this network`, [
-        `This is not natively used with the current kind of proxy ('beacon').`,
-        `Changes to the admin will have no effect on this new proxy.`,
-      ]);
-    }
+    const { impl } = await deployImplForBeacon(hre, ImplFactory, opts);
 
     let beaconDeployment: Required<BeaconDeployment & DeployTransaction>;
 
     const UpgradeableBeaconFactory = await getUpgradeableBeaconFactory(hre, ImplFactory.signer);
-    //beaconDeployment = await deploy(UpgradeableBeaconFactory, impl);
     beaconDeployment = await deploy(UpgradeableBeaconFactory, impl);
-    //const BeaconProxyFactory = await getBeaconProxyFactory(hre, ImplFactory.signer);
-    //proxyDeployment = Object.assign({ kind }, await deploy(BeaconProxyFactory, beaconDeployment.address, data));
 
     await manifest.addBeacon(beaconDeployment);
 
@@ -52,5 +38,4 @@ export function makeDeployBeacon(hre: HardhatRuntimeEnvironment): DeployBeaconFu
     beacon.deployTransaction = beaconDeployment.deployTransaction;
     return beacon;
   };
-
 }

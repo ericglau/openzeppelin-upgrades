d1c0bc51c2a91d60a015002aa835426a91d23ffc
Eric Lau <ericglau@outlook.com>
2021-11-10 20:44:41 -0500

    Add expected testcase

--- packages/plugin-hardhat/test/beacon-000-externally-deployed.js
+++ packages/plugin-hardhat/test/beacon-000-externally-deployed.js
@@ -13,6 +13,50 @@ const WAS_NOT_FOUND_IN_MANIFEST = 'was not found in the network manifest';
 
 // These tests need to run before the other beacon tests so that the beacon address will not already be in the manifest.
 
+test('deploy proxy after external beacon upgrade', async t => {
+  const { Greeter, GreeterV2, Beacon } = t.context;
+
+  // beacon with impl 1
+  const greeterBeacon = await upgrades.deployBeacon(Greeter);
+
+  // impl 2
+  const greeter2 = await GreeterV2.deploy();
+  await greeter2.deployed();
+
+  // upgrade beacon to impl 2
+  const beaconContract = Beacon.attach(greeterBeacon.address);
+  await beaconContract.upgradeTo(greeter2.address);
+
+  // deploy beacon proxy to attach to beacon (which points to impl 2)
+  const greeterProxy = await upgrades.deployBeaconProxy(greeterBeacon, GreeterV2, ['Hello, Hardhat!']);
+  await greeterProxy.deployed();
+  t.is(await greeterProxy.greet(), 'Hello, Hardhat!');
+  await greeterProxy.resetGreeting();
+  t.is(await greeterProxy.greet(), 'Hello World');
+});
+
+test('deploy proxy using beacon address after external beacon upgrade', async t => {
+  const { Greeter, GreeterV2, Beacon } = t.context;
+
+  // beacon with impl 1
+  const greeterBeacon = await upgrades.deployBeacon(Greeter);
+
+  // impl 2
+  const greeter2 = await GreeterV2.deploy();
+  await greeter2.deployed();
+
+  // upgrade beacon to impl 2
+  const beaconContract = Beacon.attach(greeterBeacon.address);
+  await beaconContract.upgradeTo(greeter2.address);
+
+  // deploy beacon proxy to attach to beacon (which points to impl 2)
+  const greeterProxy = await upgrades.deployBeaconProxy(greeterBeacon.address, GreeterV2.signer, ['Hello, Hardhat!']);
+  await greeterProxy.deployed();
+  t.is(await greeterProxy.greet(), 'Hello, Hardhat!');
+  await greeterProxy.resetGreeting();
+  t.is(await greeterProxy.greet(), 'Hello World');
+});
+
 test('block upgrade to unregistered beacon', async t => {
   const { Greeter, GreeterV2, Beacon } = t.context;
 

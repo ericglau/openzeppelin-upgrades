1d96e8c4f7369b6bdb34b8743fa6530fc48c8010
Eric Lau <ericglau@outlook.com>
2021-12-14 17:52:32 -0500

    fixes

--- packages/core/src/beacon.ts
+++ packages/core/src/beacon.ts
@@ -1,14 +1,25 @@
-import { call, EIP1967BeaconNotFound, EIP1967ImplementationNotFound, getBeaconAddress, getImplementationAddress } from '.';
+import { call, EIP1967BeaconNotFound, EIP1967ImplementationNotFound, getBeaconAddress, getImplementationAddress, UpgradesError } from '.';
 
 import { EthereumProvider } from './provider';
-import { addHexPrefix, isValidAddress, toChecksumAddress, unpadHexString } from 'ethereumjs-util';
+import { parseAddress } from './utils/address';
+
+export class InvalidBeacon extends UpgradesError {}
 
 export async function getImplementationAddressFromBeacon1(provider: EthereumProvider, beaconAddress: string): Promise<any> {
-  const result = await call(provider, beaconAddress);
-  const unpad = unpadHexString(result);
+  const implAddress = await call(provider, beaconAddress);
+  /*const unpad = unpadHexString(result);
   const prefixed = addHexPrefix(unpad);
   const checksummed = toChecksumAddress(prefixed);
-  return checksummed;
+  return checksummed;*/
+  return parseImplementationAddress(beaconAddress, implAddress);
+}
+
+function parseImplementationAddress(beaconAddress: string, implAddress: string): string {
+  const address = parseAddress(implAddress);
+  if (address === undefined) {
+    throw new InvalidBeacon(`Contract at ${beaconAddress} doesn't look like a beacon`);
+  }
+  return address;
 }
 
 /**
@@ -18,9 +29,9 @@ export async function getImplementationAddressFromBeacon1(provider: EthereumProv
  */
  export async function isBeacon(provider: EthereumProvider, beaconAddress: string) {
   try {
-    return isValidAddress(await getImplementationAddressFromBeacon1(provider, beaconAddress));
+    return await getImplementationAddressFromBeacon1(provider, beaconAddress);
   } catch (e: any) {
-    if (e.message.includes('function selector was not recognized') || e.message.includes('call revert exception')) {
+    if (e.message.includes('function selector was not recognized') || e.message.includes('call revert exception') || e instanceof InvalidBeacon) {
       return false;
     } else {
       throw e;

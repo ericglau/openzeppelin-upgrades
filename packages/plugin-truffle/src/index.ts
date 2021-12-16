1899142a7bcdb96a13966f6ff8f9f0596b6bcf78
Eric Lau <ericglau@outlook.com>
2021-12-15 22:26:30 -0500

    Fixes

--- packages/plugin-truffle/src/index.ts
+++ packages/plugin-truffle/src/index.ts
@@ -3,5 +3,6 @@ export { silenceWarnings } from '@openzeppelin/upgrades-core';
 export * from './deploy-proxy';
 export * from './prepare-upgrade';
 export * from './upgrade-proxy';
+export { deployBeacon } from './deploy-beacon';
 export { admin } from './admin';
 export { erc1967 } from './erc1967';

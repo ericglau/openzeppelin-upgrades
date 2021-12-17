2a94ca82d03c161f1e05c5d3e094563219714607
Eric Lau <ericglau@outlook.com>
2021-12-17 16:24:54 -0500

    fix

--- packages/plugin-truffle/src/upgrade-beacon.ts
+++ packages/plugin-truffle/src/upgrade-beacon.ts
@@ -2,12 +2,10 @@ import {
   ContractAddressOrInstance,
   ContractClass,
   ContractInstance,
-  deploy,
   deployBeaconImpl,
   getContractAddress,
   getUpgradeableBeaconFactory,
   Options,
-  withDefaults,
 } from './utils';
 
 export async function upgradeBeacon(beacon: ContractAddressOrInstance, Contract: ContractClass, opts: Options = {}): Promise<ContractInstance> {
@@ -15,18 +13,7 @@ export async function upgradeBeacon(beacon: ContractAddressOrInstance, Contract:
   const { impl: nextImpl } = await deployBeaconImpl(Contract, opts);
 
   const UpgradeableBeaconFactory = getUpgradeableBeaconFactory(Contract);
-// TODO just copy from Contract
   const beaconContract = new UpgradeableBeaconFactory(beaconAddress);
-//  UpgradeableBeaconFactory.detectNetwork();
-//  UpgradeableBeaconFactory.address = beaconAddress;
-//  const beaconContract = await UpgradeableBeaconFactory.deployed();
   await beaconContract.upgradeTo(nextImpl);
-  
-  //Contract.address = beaconAddress;
-  
-  //const beaconContract = new Contract(beaconDeployment.address);
-  //beaconContract.transactionHash = beaconDeployment.txHash;
-  
-  //return new Contract(beaconAddress);
   return beaconContract;
 }

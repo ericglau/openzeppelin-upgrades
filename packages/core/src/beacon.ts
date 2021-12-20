9edadf74c0b6a3d737932a8bdbba9051db7aedc2
Eric Lau <ericglau@outlook.com>
2021-12-20 17:40:31 -0500

    fixes

--- packages/core/src/beacon.ts
+++ packages/core/src/beacon.ts
@@ -5,7 +5,7 @@ import { EthereumProvider } from './provider';
 /**
  * Checks if the address looks like a beacon.
  *
- * @returns true if the address has an implementation() function that returns an address, false otherwise.
+ * @returns Promise<true> if the address has an implementation() function that returns an address, Promise<false> otherwise.
  */
 export async function isBeacon(provider: EthereumProvider, beaconAddress: string): Promise<boolean> {
   try {

7db47745bc176bc579e2cb3f2d366d7951bad7e1
Eric Lau <ericglau@outlook.com>
2021-12-19 22:35:59 -0500

    partially working test

--- packages/plugin-truffle/test/test/beacon-happy-path.js
+++ packages/plugin-truffle/test/test/beacon-happy-path.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 
-const { deployBeacon, deployBeaconProxy, upgradeBeacon, prepareUpgrade } = require('@openzeppelin/truffle-upgrades');
+const { deployBeacon, deployBeaconProxy, upgradeBeacon, loadProxy, prepareUpgrade } = require('@openzeppelin/truffle-upgrades');
 
 const Greeter = artifacts.require('Greeter');
 const GreeterV2 = artifacts.require('GreeterV2');
@@ -64,8 +64,30 @@ contract('Greeter', function () {
       throw new Error(`expected Hello Truffle but got ${await greeter.greet()}`);
     }
 
-    await upgradeBeacon(greeterBeacon, GreeterV2);
+    const greeterSecond = await deployBeaconProxy(greeterBeacon, ['Hello Truffle second']);
+    assert.ok(greeterSecond.transactionHash, 'transaction hash is missing');
+    if (await greeterSecond.greet() !== 'Hello Truffle second') {
+      throw new Error(`expected Hello Truffle second but got ${await greeterSecond.greet()}`);
+    }
 
+    //  new impl
+    await upgradeBeacon(greeterBeacon, GreeterV2);
+    if (await greeter.greet() !== 'Hello Truffle') {
+      throw new Error(`expected Hello Truffle but got ${await greeter.greet()}`);
+    }
+
+    // reload proxy to work with the new contract
+    const greeter2 = await GreeterV2.at(greeter.address);//await loadProxy(greeter);
+    if (await greeter2.greet() !== 'Hello Truffle') {
+      throw new Error(`expected Hello Truffle but got ${await greeter.greet()}`);
+    }
+
+    await greeter2.resetGreeting();
+    if (await greeter2.greet() !== 'Hello World') {
+      throw new Error(`expected Hello World but got ${await greeter2.greet()}`);
+    }
+
+    // prepare upgrade from beacon proxy
     const greeter3ImplAddr = await prepareUpgrade(greeterBeacon.address, GreeterV3);
     const greeter3 = await GreeterV3.at(greeter3ImplAddr);
     const version3 = await greeter3.version();

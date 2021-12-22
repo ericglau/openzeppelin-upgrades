0adebee950189bfad63b12f0c158d3d6285cc6e6
Eric Lau <ericglau@outlook.com>
2021-12-22 17:33:12 -0500

    more tests

---
+++ packages/plugin-truffle/test/test/load-proxy.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+const upgrades = require('@openzeppelin/truffle-upgrades');
+
+const Greeter = artifacts.require('Greeter');
+const GreeterProxiable = artifacts.require('GreeterProxiable');
+
+it('TODO', async function () {
+//TODO
+});

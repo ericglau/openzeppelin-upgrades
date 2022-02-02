3e26dca86a38643220a7ab43d5a2c38820f71948
Eric Lau <ericglau@outlook.com>
2022-02-02 18:08:23 -0500

    determine kind from bytecode

--- packages/plugin-hardhat/src/import-proxy.ts
+++ packages/plugin-hardhat/src/import-proxy.ts
@@ -1,4 +1,4 @@
-import type { HardhatRuntimeEnvironment } from 'hardhat/types';
+import type {  HardhatRuntimeEnvironment } from 'hardhat/types';
 import type { ContractFactory, Contract } from 'ethers';
 
 import {
@@ -12,6 +12,7 @@ import {
   implLens,
   getCode,
   getVersion,
+  EthereumProvider,
 } from '@openzeppelin/upgrades-core';
 
 import {
@@ -24,6 +25,7 @@ import {
   deployProxyImpl,
   getInitializerData,
   getDeployData,
+  getBeaconProxyFactory,
 } from './utils';
 import { FormatTypes } from 'ethers/lib/utils';
 
@@ -34,7 +36,7 @@ export interface ImportProxyFunction {
 function isProbableMatch(creationCode: string, deployedBytecode: string) {
   const creationCodeWithoutPrefix = creationCode.replace(/^0x/, '');
   const deployedBytecodeWithoutPrefix = deployedBytecode.replace(/^0x/, '');
-  return creationCodeWithoutPrefix.endsWith(deployedBytecodeWithoutPrefix);
+  return creationCodeWithoutPrefix.includes(deployedBytecodeWithoutPrefix);
 }
 
 export function makeImportProxy(hre: HardhatRuntimeEnvironment): ImportProxyFunction {
@@ -55,64 +57,52 @@ export function makeImportProxy(hre: HardhatRuntimeEnvironment): ImportProxyFunc
 
 
     // from deploy-impl
-    const deployData = await getDeployData(hre, ImplFactory, opts); // TODO move this stuff to deploy-impl so this function doesn't need to be exported
-    const layout = deployData.layout;
-    const deployTx = async () => {
-      const abi = ImplFactory.interface.format(FormatTypes.minimal) as string[];
-      const deployment = Object.assign({ abi });//, await deploy(ImplFactory /* no contructor args //, ...deployData.fullOpts.constructorArgs*/));
-      return { ...deployment, layout, address: impl }; // TODO check where we should actually put this address part
-    }
+    const { deployTx, deployData } = await getDeploymentFromImpl(hre, ImplFactory, opts, impl);
 
-    //const proxyDeployment: ProxyDeployment & DeployTransaction = { kind: 'uups', address: proxyAddress, /*, txHash: '0x'*/ deployTransaction: deployment };
-    //const proxyDeployment: ProxyDeployment = { kind: 'uups', address: proxyAddress };
-    const proxyDeployment: ProxyDeployment & DeployTransaction = { kind: 'uups', address: proxyAddress, deployTransaction: await deployTx() };
+
+
+    // get proxy type from bytecode
+    let kind : ProxyDeployment["kind"];
+    if (await isBytecodeMatch(provider, proxyAddress, await getProxyFactory(hre, ImplFactory.signer))) {
+      kind = 'uups';
+    } else if (await isBytecodeMatch(provider, proxyAddress, await getTransparentUpgradeableProxyFactory(hre, ImplFactory.signer))) {
+      kind = 'transparent';
+    } else if (await isBytecodeMatch(provider, proxyAddress, await getBeaconProxyFactory(hre, ImplFactory.signer))) {
+      kind = 'beacon';
+    } else {
+      throw new Error("cannot determine proxy type"); // TODO let user specify it
+    }
+    console.log("determined kind " + kind);
+
+
+    const proxyToImport: ProxyDeployment = { kind: kind , address: proxyAddress };
     
 
 
-
-    // check if bytecode matches
-    console.log("Input CF bytecode:\n" + ImplFactory.bytecode);
-    // console.log("Input version withMetadata:\n" + deployData.version.withMetadata);
-    // console.log("Input version withoutMetadata:\n" + deployData.version.withoutMetadata);
-    // console.log("Input version linkedWithoutMetadata:\n" + deployData.version.linkedWithoutMetadata);
-
-    const implBytecode = await getCode(provider, impl);
-    console.log("Read deployed bytecode:\n" + implBytecode);
-
-    const probableMatch = isProbableMatch(ImplFactory.bytecode, implBytecode);
-    console.log("probable match?:\n" + probableMatch);
-
-    if (!probableMatch) {
-      throw new Error("Contract does not match with implementation bytecode deployed at " + impl);
-    }
-
-    // const deployedVersion = getVersion(implBytecode, deployData.encodedArgs);
-    // console.log("Read deployed version withMetadata:\n" + deployedVersion.withMetadata);
-    // console.log("Read deployed version withoutMetadata:\n" + deployedVersion.withoutMetadata);
-    // console.log("Read deployed version linkedWithoutMetadata:\n" + deployedVersion.linkedWithoutMetadata);
-
-    await manifest.addProxy(proxyDeployment);
-
-    const lens = implLens(deployData.version.linkedWithoutMetadata);
-    //try {
-      const deployment1 = await manifest.lockedRun(async () => {
-        const data = await manifest.read();
-        const deployment = lens(data);
-        const updated = await deployTx();//await resumeOrDeploy(provider, stored, deploy);
-       // if (updated !== stored) {
-          //await checkForAddressClash(provider, data, updated);
-          deployment.set(updated);
-          await manifest.write(data);
-        //}
-        return updated;
-      });
-      
-
-
-    // basically return Greeter.attach(proxy.address);
+    await checkBytecodeMatch(provider, impl, ImplFactory);
+    await updateManifest();
     return ImplFactory.attach(proxyAddress);
 
 
+
+    async function updateManifest() {
+      await manifest.addProxy(proxyToImport);
+      const lens = implLens(deployData.version.linkedWithoutMetadata);
+      await manifest.lockedRun(async () => {
+        const data = await manifest.read();
+        const deployment = lens(data);
+        const stored = deployment.get();
+        const updated = await deployTx(); //await resumeOrDeploy(provider, stored, deploy);
+
+        if (updated !== stored) {
+          //await checkForAddressClash(provider, data, updated); // TODO not sure if we need this
+          // TODO if there is an existing impl version at different address, we should use that one (?)
+          deployment.set(updated);
+          await manifest.write(data);
+        }
+        return updated;
+      });
+    }
     // TODO write impl to manifest
     // TODO the below is from impl-store.ts
     // try {
@@ -183,3 +173,31 @@ export function makeImportProxy(hre: HardhatRuntimeEnvironment): ImportProxyFunc
     //return inst;
   };
 }
+
+async function checkBytecodeMatch(provider: EthereumProvider, impl: string, ImplFactory: ContractFactory) {
+  const probableMatch = await isBytecodeMatch(provider, impl, ImplFactory);
+  if (!probableMatch) {
+    throw new Error("Contract does not match with implementation bytecode deployed at " + impl);
+  }
+}
+
+async function isBytecodeMatch(provider: EthereumProvider, addr: string, Factory: ContractFactory) {
+  const implBytecode = await getCode(provider, addr);
+  //console.log("factory bytecode " + Factory.bytecode);
+  //console.log("addr bytecode " + implBytecode);
+
+  const probableMatch = isProbableMatch(Factory.bytecode, implBytecode);
+  return probableMatch;
+}
+
+async function getDeploymentFromImpl(hre: HardhatRuntimeEnvironment, ImplFactory: ContractFactory, opts: DeployProxyOptions, impl: string) {
+  const deployData = await getDeployData(hre, ImplFactory, opts); // TODO move this stuff to deploy-impl so this function doesn't need to be exported
+  const layout = deployData.layout;
+  const deployTx = async () => {
+    const abi = ImplFactory.interface.format(FormatTypes.minimal) as string[];
+    const deployment = Object.assign({ abi }); //, await deploy(ImplFactory /* no contructor args //, ...deployData.fullOpts.constructorArgs*/));
+    return { ...deployment, layout, address: impl }; // TODO check where we should actually put this address part
+  };
+  return { deployTx, deployData };
+}
+

fdd797218dadebe29c1facd32ed34ab70105c0d9
Eric Lau <ericglau@outlook.com>
2022-02-05 13:38:38 -0500

    add warning for admin

--- packages/plugin-hardhat/src/utils/simulate-deploy.ts
+++ packages/plugin-hardhat/src/utils/simulate-deploy.ts
@@ -1,5 +1,5 @@
 import {
-  fetchOrDeploy, fetchOrDeployAdmin,
+  fetchOrDeploy, fetchOrDeployAdmin, logWarning,
 } from '@openzeppelin/upgrades-core';
 import type { ContractFactory } from 'ethers';
 import { FormatTypes } from 'ethers/lib/utils';
@@ -10,9 +10,12 @@ import { Options } from './options';
 export async function simulateDeployAdmin(hre: HardhatRuntimeEnvironment, ProxyAdminFactory: ContractFactory, opts: Options, adminAddress: string) {
   const { deployData, simulateDeploy } = await simulateDeployment(hre, ProxyAdminFactory, opts, adminAddress);
   const manifestAdminAddress = await fetchOrDeployAdmin(deployData.provider, simulateDeploy, opts);
-  // TODO give warning if imported admin differs from manifest
   if (adminAddress !== manifestAdminAddress) {
-    throw new Error("admin address does not match manifest admin address"); // TODO change this to a warning
+    // TODO test this
+    logWarning(`Imported proxy with admin at '${adminAddress}' which differs from previously deployed admin '${manifestAdminAddress}'`, [
+      `The imported proxy admin is different than the proxy admin that was previously deployed on this network. This proxy will not be upgradable directly by the plugin.`,
+      `To upgrade this proxy, use the prepareProxy or defender.proposeUpgrade function and then upgrade it using the admin at '${adminAddress}'.`
+    ]);
   }
 }
 
@@ -26,14 +29,14 @@ async function simulateDeployment(hre: HardhatRuntimeEnvironment,
   opts: Options,
   implAddress: string) {
   const deployData = await getDeployData(hre, ImplFactory, opts);
-  const simulateDeploy = await getSimulateDeployFunction(deployData, ImplFactory, implAddress);
+  const simulateDeploy = await getSimulateDeploy(deployData, ImplFactory, implAddress);
   return { deployData, simulateDeploy };
 }
 
 /**
  * Gets a function that returns a simulated deployment of the given contract to the given address.
  */
-async function getSimulateDeployFunction(deployData: DeployData, contractFactory: ContractFactory, addr: string) {
+async function getSimulateDeploy(deployData: DeployData, contractFactory: ContractFactory, addr: string) {
   const simulateDeploy = async () => {
     const abi = contractFactory.interface.format(FormatTypes.minimal) as string[];
     const deployment = Object.assign({ abi });

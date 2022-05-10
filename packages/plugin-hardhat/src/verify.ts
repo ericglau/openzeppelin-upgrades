047d415080ffd9085fcee1afc2e7f80998fb2997
Eric Lau <ericglau@outlook.com>
2022-05-10 15:55:13 -0400

    Improvements - working

--- packages/plugin-hardhat/src/verify.ts
+++ packages/plugin-hardhat/src/verify.ts
@@ -1,14 +1,19 @@
 import { resolveEtherscanApiKey } from '@nomiclabs/hardhat-etherscan/dist/src/resolveEtherscanApiKey';
 import { toCheckStatusRequest, toVerifyRequest } from '@nomiclabs/hardhat-etherscan/dist/src/etherscan/EtherscanVerifyContractRequest';
 import { delay, getVerificationStatus, verifyContract } from '@nomiclabs/hardhat-etherscan/dist/src/etherscan/EtherscanService';
-import { getProxyFactory } from './utils/factories';
-
-import ERC1967Proxy from '@openzeppelin/upgrades-core/artifacts/@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol/ERC1967Proxy.json';
+import { getBeaconProxyFactory, getProxyFactory, getTransparentUpgradeableProxyFactory } from './utils/factories';
 
 import { Manifest, getTransactionByHash, getImplementationAddress, EIP1967ImplementationNotFound, getBeaconAddress, getImplementationAddressFromBeacon, EIP1967BeaconNotFound, UpgradesError } from '@openzeppelin/upgrades-core';
 import { EthereumProvider, HardhatRuntimeEnvironment, RunSuperFunction } from 'hardhat/types';
 import { EtherscanConfig } from '@nomiclabs/hardhat-etherscan/dist/src/types';
-import { NomicLabsHardhatPluginError } from 'hardhat/plugins';
+import { ContractFactory } from 'ethers';
+
+import ERC1967Proxy from '@openzeppelin/upgrades-core/artifacts/@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol/ERC1967Proxy.json';
+import BeaconProxy from '@openzeppelin/upgrades-core/artifacts/@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol/BeaconProxy.json';
+import UpgradeableBeacon from '@openzeppelin/upgrades-core/artifacts/@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol/UpgradeableBeacon.json';
+import TransparentUpgradeableProxy from '@openzeppelin/upgrades-core/artifacts/@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol/TransparentUpgradeableProxy.json';
+import ProxyAdmin from '@openzeppelin/upgrades-core/artifacts/@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol/ProxyAdmin.json';
+
 
 const buildInfo = require('@openzeppelin/upgrades-core/artifacts/build-info.json');
 
@@ -34,65 +39,110 @@ async function getTransactionHashFromManifest(provider: EthereumProvider, proxyA
 export async function verify(args: any, hre: HardhatRuntimeEnvironment, runSuper: RunSuperFunction<any>) {
   const provider = hre.network.provider;
   let addresses: Addresses = await getRelatedAddresses(provider, args.address);
+  console.log(`Addresses: ${JSON.stringify(addresses)}`);
 
   if (addresses.impl === undefined) {
     // does not look like a proxy, so just verify directly
     return hardhatVerify(args.address);
   } else {
     const proxyAddress = args.address;
-    console.log(`Detected proxy ${proxyAddress.impl}`);
+    console.log(`Detected proxy ${proxyAddress}`);
 
     console.log(`Verifying implementation ${addresses.impl}`);
-    hardhatVerify(addresses.impl); // TODO if already verified, continue
-    console.log(`Implementation ${addresses.impl} verified!`);
+    try {
+      await hardhatVerify(addresses.impl);
+      console.log(`Implementation ${addresses.impl} verified!`);
+    } catch (e: any) {
+      if (e.message.includes('already verified')) {
+        console.log(`Implementation ${addresses.impl} already verified.`);
+      } else {
+        throw e;
+      }
+    }
   
     let etherscanApi: EtherscanAPI = await getEtherscanAPI(hre);
-  
-    let constructorArguments = await getConstructorArgs(hre, proxyAddress);
-  
-    const params = {
-      apiKey: etherscanApi.key,
-      contractAddress: proxyAddress,
-      sourceCode: JSON.stringify(buildInfo.input),
-      sourceName: ERC1967Proxy.sourceName,
-      contractName: ERC1967Proxy.contractName,
-      compilerVersion:  `v${buildInfo.solcLongVersion}`,
-      constructorArguments: constructorArguments,
-    }
-    
-    const request = toVerifyRequest(params);
-    const response = await verifyContract(etherscanApi.endpoints.urls.apiURL, request);
-    const pollRequest = toCheckStatusRequest({
-      apiKey: etherscanApi.key,
-      guid: response.message,
-    });
-  
-    // Compilation is bound to take some time so there's no sense in requesting status immediately.
-    await delay(700);
-    try {
-      const verificationStatus = await getVerificationStatus(
-        etherscanApi.endpoints.urls.apiURL,
-        pollRequest
-      );
-      
-      if (
-        verificationStatus.isVerificationFailure() ||
-        verificationStatus.isVerificationSuccess()
-      ) {
-        console.log(`Verification status for ${proxyAddress}: ${verificationStatus.message}`);
+
+    if (addresses.beacon !== undefined) {
+      // it is a beacon proxy
+      const artifact: ContractArtifactJson = BeaconProxy;
+      const constructorArguments = await getConstructorArgs(hre, await getFactory(hre, artifact), proxyAddress);
+      if (constructorArguments === undefined) {
+        console.log("The proxy contract bytecode differs than the version defined in the OpenZeppelin Upgrades Plugin. Verifying directly instead...");
+        return await hardhatVerify(args.address);
+      } else {
+        return await verifyProxy(etherscanApi, proxyAddress, constructorArguments, artifact);
       }
-    } catch (e: any) {
-      console.log(`Verification for ${proxyAddress} failed: ${e.message}`);
+    } else {
+      let artifact: ContractArtifactJson = ERC1967Proxy;
+      let constructorArguments = await getConstructorArgs(hre, await getFactory(hre, artifact), proxyAddress);
+      console.log(`UUPS: got constructor args ${constructorArguments}`);
+      if (constructorArguments === undefined) {
+        artifact = TransparentUpgradeableProxy;
+        constructorArguments = await getConstructorArgs(hre, await getFactory(hre, artifact), proxyAddress);
+        console.log(`Transparent: got constructor args ${constructorArguments}`);
+        if (constructorArguments === undefined) {
+          console.log("The proxy contract bytecode differs than the version defined in the OpenZeppelin Upgrades Plugin. Verifying directly instead...");
+          return await hardhatVerify(args.address);
+        }
+      }
+      return await verifyProxy(etherscanApi, proxyAddress, constructorArguments, artifact);
     }
-    
   }
 
-  function hardhatVerify(address: string) {
-    return runSuper({ ...args, address });
+  async function hardhatVerify(address: string) {
+    return await runSuper({ ...args, address });
   }
 }
 
-async function getConstructorArgs(hre: HardhatRuntimeEnvironment, proxyAddress: any) {
+interface ContractArtifactJson { 
+  contractName: string;
+  sourceName: string;
+  abi: any;
+  bytecode: any;
+}
+
+async function getFactory(hre: HardhatRuntimeEnvironment, contractImport: ContractArtifactJson): Promise<ContractFactory> {
+  return hre.ethers.getContractFactory(contractImport.abi, contractImport.bytecode, undefined);
+}
+
+async function verifyProxy(etherscanApi: EtherscanAPI, proxyAddress: any, constructorArguments: string, contractImport: ContractArtifactJson) {
+  console.log(`Verifying proxy ${proxyAddress} with constructor args ${constructorArguments}...`);
+
+  const params = {
+    apiKey: etherscanApi.key,
+    contractAddress: proxyAddress,
+    sourceCode: JSON.stringify(buildInfo.input),
+    sourceName: contractImport.sourceName,
+    contractName: contractImport.contractName,
+    compilerVersion: `v${buildInfo.solcLongVersion}`,
+    constructorArguments: constructorArguments,
+  };
+
+  const request = toVerifyRequest(params);
+  const response = await verifyContract(etherscanApi.endpoints.urls.apiURL, request);
+  const pollRequest = toCheckStatusRequest({
+    apiKey: etherscanApi.key,
+    guid: response.message,
+  });
+
+  // Compilation is bound to take some time so there's no sense in requesting status immediately.
+  await delay(700);
+  try {
+    const verificationStatus = await getVerificationStatus(
+      etherscanApi.endpoints.urls.apiURL,
+      pollRequest
+    );
+
+    if (verificationStatus.isVerificationFailure() ||
+      verificationStatus.isVerificationSuccess()) {
+      console.log(`Verification status for ${proxyAddress}: ${verificationStatus.message}`);
+    }
+  } catch (e: any) {
+    console.log(`Verification for ${proxyAddress} failed: ${e.message}`);
+  }
+}
+
+async function getConstructorArgs(hre: HardhatRuntimeEnvironment, proxyFactory: ContractFactory, proxyAddress: any) {
   const txHash = await getTransactionHashFromManifest(hre.network.provider, proxyAddress);
   if (txHash === undefined) {
     // TODO get constructor args from user input
@@ -102,15 +152,18 @@ async function getConstructorArgs(hre: HardhatRuntimeEnvironment, proxyAddress: 
 
   // Determine contract based on whether transaction deployment code starts with the contract's creation code 
 
-  const uupsProxyFactory = await getProxyFactory(hre);
-  const creationCode = uupsProxyFactory.bytecode;
-  console.log("UUPS creation code: " + creationCode);
+
+  const creationCode = proxyFactory.bytecode;
+  console.log("Creation code: " + creationCode);
 
   const tx = await getTransactionByHash(hre.network.provider, txHash);
-  const txInput = tx?.input;
+  if (tx === null) {
+    return undefined;
+  }
+  const txInput = tx.input;
   console.log("TX deploy code: " + txInput);
-
-  return await inferConstructorArgs(txHash, creationCode);
+  
+  return inferConstructorArgs(txInput, creationCode);
 }
 
 /**
@@ -120,10 +173,12 @@ async function getConstructorArgs(hre: HardhatRuntimeEnvironment, proxyAddress: 
  * @param creationCode The contract creation code.
  * @returns the encoded constructor args, or undefined if txInput does not start with the creationCode.
  */
-async function inferConstructorArgs(txInput: string, creationCode: string) {
+function inferConstructorArgs(txInput: string, creationCode: string) {
   if (txInput.startsWith(creationCode)) {
+    console.log(`Returning constructor args ${txInput.substring(creationCode.length)}`);
     return txInput.substring(creationCode.length);
   } else {
+    console.log(`txinput ${txInput} does not start with creation code ${creationCode}`);
     return undefined;
   }
 }

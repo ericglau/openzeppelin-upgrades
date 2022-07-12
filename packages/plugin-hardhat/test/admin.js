50fdb15e440a49278f51a1ee5ac3e3eb08a96ecd
Eric Lau <ericglau@outlook.com>
2022-07-12 18:12:01 -0400

    doc deployProxyAdmin

--- packages/plugin-hardhat/test/admin.js
+++ packages/plugin-hardhat/test/admin.js
@@ -10,8 +10,12 @@ test('deployProxyAdmin', async t => {
   const { Greeter } = t.context;
 
   await t.throwsAsync(upgrades.admin.getInstance(), undefined, 'No ProxyAdmin was found in the network manifest');
+
   const deployedAdminAddress = await upgrades.deployProxyAdmin();
-  const deployedAdminAddress2 = await upgrades.deployProxyAdmin();
+
+  const signer = (await ethers.getSigners())[1];
+  const deployedAdminAddress2 = await upgrades.deployProxyAdmin(signer);
+
   t.is(deployedAdminAddress2, deployedAdminAddress);
 
   const adminInstance = await upgrades.admin.getInstance();

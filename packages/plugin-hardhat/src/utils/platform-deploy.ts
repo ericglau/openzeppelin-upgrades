54db03e1257fc06df74e4b28e827a01e0c935fc0
Eric Lau <ericglau@outlook.com>
2023-03-09 08:53:40 -0500

    Get network name

--- packages/plugin-hardhat/src/utils/platform-deploy.ts
+++ packages/plugin-hardhat/src/utils/platform-deploy.ts
@@ -1,10 +1,10 @@
-import { Deployment, getChainId } from '@openzeppelin/upgrades-core';
+import { Deployment, getChainId, UpgradesError } from '@openzeppelin/upgrades-core';
 import type { ethers, ContractFactory } from 'ethers';
 
 import { promises as fs } from 'fs';
 
 import { PlatformClient, SourceCodeLicense } from 'platform-deploy-client';
-import { Network } from 'defender-base-client';
+import { Network, fromChainId } from 'platform-deploy-client/node_modules/defender-base-client'; // TODO fix dependencies
 
 import { BuildInfo, HardhatRuntimeEnvironment } from 'hardhat/types';
 
@@ -45,12 +45,13 @@ export async function platformDeploy(
   console.log("BEFORE PLATFORM DEPLOY");
   console.log(contractName, sourceName);
 
-  throw new Error("GOT NETWORK " + await getNetwork(hre));
+  const network = await getNetwork(hre);
+  console.log("USING NETWORK NAME " + network);
   
   const deploymentResponse = await client.Deployment.deploy({
     contractName: contractName,
     contractPath: sourceName,
-    network: 'mumbai', // TODO
+    network: network,
     artifactPayload: JSON.stringify(buildInfo),
     licenseType: getLicense(buildInfo, sourceName, contractName),
     constructorInputs: constructorArgs,
@@ -65,36 +66,13 @@ export async function platformDeploy(
 async function getNetwork(hre: HardhatRuntimeEnvironment) : Promise<Network> {
   const { provider } = hre.network;
   let chainId = hre.network.config.chainId ?? await getChainId(provider);
-  console.log("DETERMINED CHAIN ID " +  chainId);
-  return getNetworkFromChainId(chainId);
-}
-
-export const networkNames: { [chainId in number]?: Network } = Object.freeze({
-  1: 'mainnet',
-  // 2: 'morden',
-  // 3: 'ropsten',
-  // 4: 'rinkeby',
-  // 5: 'goerli',
-  // 10: 'optimism',
-  // 42: 'kovan',
-  // 56: 'bsc',
-  // 97: 'bsc-testnet',
-  // 137: 'polygon',
-  // 420: 'optimism-goerli',
-  80001: 'mumbai',
-  // 43113: 'avalanche-fuji',
-  // 43114: 'avalanche',
-  // 42220: 'celo',
-  // 44787: 'celo-alfajores',
-});
-
-function getNetworkFromChainId(chainId: number): Network {
-//  type PublicNetwork = 'mainnet' | 'goerli' | 'xdai' | 'sokol' | 'fuse' | 'bsc' | 'bsctest' | 'fantom' | 'fantomtest' | 'moonbase' | 'moonriver' | 'moonbeam' | 'matic' | 'mumbai' | 'avalanche' | 'fuji' | 'optimism' | 'optimism-goerli' | 'arbitrum' | 'arbitrum-goerli' | 'celo' | 'alfajores' | 'harmony-s0' | 'harmony-test-s0' | 'aurora' | 'auroratest' | 'hedera' | 'hederatest' | 'zksync-goerli';
-  const networkName = networkNames[chainId];
-  if (networkName === undefined) {
-    throw new Error(`Could not identify network name for chainId ${chainId}`);
+  console.log("GOT CHAIN ID " +  chainId);
+  const network = fromChainId(chainId);
+  if (network === undefined) {
+    throw new UpgradesError(`Could not get network name from chain id ${chainId}.`,
+      () => "Ensure you are using a network that is supported by Platform.");
   }
-  return networkName;
+  return network;
 }
 
 function getLicense(buildInfo: BuildInfo | undefined, sourceName: string, contractName: string): SourceCodeLicense | undefined {
@@ -103,7 +81,7 @@ function getLicense(buildInfo: BuildInfo | undefined, sourceName: string, contra
     const content = buildInfo.output.contracts[sourceName][contractName];
     const metadataString = (content as any).metadata;
     const metadata = JSON.parse(metadataString);
-    console.log("metadata " + JSON.stringify(metadata.sources, null, 2));
+    // console.log("metadata " + JSON.stringify(metadata.sources, null, 2));
     const license = metadata.sources[sourceName].license;
     console.log("found license " + license);
     return license;

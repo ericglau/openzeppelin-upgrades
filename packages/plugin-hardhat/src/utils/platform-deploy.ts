ff04aa09f3d042048dae4d83efbf4e2f59b595de
Eric Lau <ericglau@outlook.com>
2023-03-09 08:38:03 -0500

    Get chain ID WIP

--- packages/plugin-hardhat/src/utils/platform-deploy.ts
+++ packages/plugin-hardhat/src/utils/platform-deploy.ts
@@ -1,9 +1,10 @@
-import type { Deployment } from '@openzeppelin/upgrades-core';
+import { Deployment, getChainId } from '@openzeppelin/upgrades-core';
 import type { ethers, ContractFactory } from 'ethers';
 
 import { promises as fs } from 'fs';
 
 import { PlatformClient, SourceCodeLicense } from 'platform-deploy-client';
+import { Network } from 'defender-base-client';
 
 import { BuildInfo, HardhatRuntimeEnvironment } from 'hardhat/types';
 
@@ -43,6 +44,8 @@ export async function platformDeploy(
   console.log("constructor args " + JSON.stringify(constructorArgs, null, 2));
   console.log("BEFORE PLATFORM DEPLOY");
   console.log(contractName, sourceName);
+
+  throw new Error("GOT NETWORK " + await getNetwork(hre));
   
   const deploymentResponse = await client.Deployment.deploy({
     contractName: contractName,
@@ -57,6 +60,41 @@ export async function platformDeploy(
   const txResponse = await hre.ethers.provider.getTransaction(deploymentResponse.txHash);
   const checksumAddress = hre.ethers.utils.getAddress(deploymentResponse.address); 
   return { address: checksumAddress, txHash: deploymentResponse.txHash, deployTransaction: txResponse };
+}
+
+async function getNetwork(hre: HardhatRuntimeEnvironment) : Promise<Network> {
+  const { provider } = hre.network;
+  let chainId = hre.network.config.chainId ?? await getChainId(provider);
+  console.log("DETERMINED CHAIN ID " +  chainId);
+  return getNetworkFromChainId(chainId);
+}
+
+export const networkNames: { [chainId in number]?: Network } = Object.freeze({
+  1: 'mainnet',
+  // 2: 'morden',
+  // 3: 'ropsten',
+  // 4: 'rinkeby',
+  // 5: 'goerli',
+  // 10: 'optimism',
+  // 42: 'kovan',
+  // 56: 'bsc',
+  // 97: 'bsc-testnet',
+  // 137: 'polygon',
+  // 420: 'optimism-goerli',
+  80001: 'mumbai',
+  // 43113: 'avalanche-fuji',
+  // 43114: 'avalanche',
+  // 42220: 'celo',
+  // 44787: 'celo-alfajores',
+});
+
+function getNetworkFromChainId(chainId: number): Network {
+//  type PublicNetwork = 'mainnet' | 'goerli' | 'xdai' | 'sokol' | 'fuse' | 'bsc' | 'bsctest' | 'fantom' | 'fantomtest' | 'moonbase' | 'moonriver' | 'moonbeam' | 'matic' | 'mumbai' | 'avalanche' | 'fuji' | 'optimism' | 'optimism-goerli' | 'arbitrum' | 'arbitrum-goerli' | 'celo' | 'alfajores' | 'harmony-s0' | 'harmony-test-s0' | 'aurora' | 'auroratest' | 'hedera' | 'hederatest' | 'zksync-goerli';
+  const networkName = networkNames[chainId];
+  if (networkName === undefined) {
+    throw new Error(`Could not identify network name for chainId ${chainId}`);
+  }
+  return networkName;
 }
 
 function getLicense(buildInfo: BuildInfo | undefined, sourceName: string, contractName: string): SourceCodeLicense | undefined {
@@ -133,3 +171,5 @@ async function getContractInfo(factory: ethers.ContractFactory, hre: HardhatRunt
   }
   return { contractName, sourceName, buildInfo };
 }
+
+

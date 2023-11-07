6301c7dc21f2fcb9fc378260296e1811c38b065c
Eric Lau <ericglau@outlook.com>
2023-11-07 14:11:43 -0500

    update transfer ownership test WIP

--- packages/plugin-hardhat/test/transparent-transfer-admin-ownership-happy-path.js
+++ packages/plugin-hardhat/test/transparent-transfer-admin-ownership-happy-path.js
@@ -1,7 +1,6 @@
 const test = require('ava');
 
 const hre = require('hardhat');
-const { getManifestAdmin } = require('@openzeppelin/hardhat-upgrades/dist/admin.js');
 const { ethers, upgrades } = hre;
 const testAddress = '0x1E6876a6C2757de611c9F12B23211dBaBd1C9028';
 
@@ -12,10 +11,18 @@ test.before(async t => {
 test('transferProxyAdminOwnership', async t => {
   // we need to deploy a proxy so we have a Proxy Admin
   const { Greeter } = t.context;
-  await upgrades.deployProxy(Greeter, ['Hello, Hardhat!'], { kind: 'transparent' });
+  const greeter = await upgrades.deployProxy(Greeter, ['Hello, Hardhat!'], { kind: 'transparent' });
 
-  const admin = await getManifestAdmin(hre);
-  await upgrades.admin.transferProxyAdminOwnership(testAddress);
+  const adminAddress = await upgrades.erc1967.getAdminAddress(await greeter.getAddress());
+
+  const signer = await ethers.getSigners()[0];
+
+  const admin = new ethers.Contract(adminAddress, [
+      'function transferOwnership(address)',
+      'function owner()',
+  ], signer);
+
+  await admin.transferOwnership(testAddress);
   const newOwner = await admin.owner();
 
   t.is(newOwner, testAddress);

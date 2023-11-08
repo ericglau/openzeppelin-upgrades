5593b7e70a54ad35643cacf633f585b7971ec8ab
Eric Lau <ericglau@outlook.com>
2023-11-08 14:13:38 -0500

    Use npm for legacy upgrades-core artifacts

--- packages/core/scripts/copy-artifacts.sh
+++ packages/core/scripts/copy-artifacts.sh
@@ -6,4 +6,10 @@ set -euo pipefail
 
 mkdir -p artifacts
 
-cp -R legacy/ artifacts
+# Assert that a previous version of @openzeppelin/upgrades-core is installed, which contains legacy artifacts
+if ! grep -q '"version": "1.31.1"' ../../node_modules/@openzeppelin/upgrades-core-legacy/package.json; then
+  echo "Error: @openzeppelin/upgrades-core must depend on a previous version of itself at version 1.31.1"
+  exit 1
+fi
+
+cp -R ../../node_modules/@openzeppelin/upgrades-core-legacy/artifacts/ artifacts

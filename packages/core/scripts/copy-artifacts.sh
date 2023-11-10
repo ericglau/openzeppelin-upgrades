5dea6768f48fe2b00d3908422112b2f241260a51
Eric Lau <ericglau@outlook.com>
2023-11-10 10:37:04 -0500

    logging

--- packages/core/scripts/copy-artifacts.sh
+++ packages/core/scripts/copy-artifacts.sh
@@ -12,4 +12,11 @@ if ! grep -q '"version": "1.31.1"' ../../node_modules/@openzeppelin/upgrades-cor
   exit 1
 fi
 
+echo "Node modules:"
+ls -la ../../node_modules/
+echo "openzeppelin dir:"
+ls -la ../../node_modules/@openzeppelin/
+echo "Legacy artifacts:"
+ls -la ../../node_modules/@openzeppelin/upgrades-core-legacy/artifacts/
+
 cp -R ../../node_modules/@openzeppelin/upgrades-core-legacy/artifacts/ artifacts

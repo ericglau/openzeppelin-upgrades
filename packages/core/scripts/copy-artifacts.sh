0cdeb7aff091940ddf3ba5d6685b305eb2842909
Eric Lau <ericglau@outlook.com>
2023-11-10 11:03:00 -0500

    cleanup

--- packages/core/scripts/copy-artifacts.sh
+++ packages/core/scripts/copy-artifacts.sh
@@ -12,18 +12,4 @@ if ! grep -q '"version": "1.31.1"' ../../node_modules/@openzeppelin/upgrades-cor
   exit 1
 fi
 
-echo "Node modules:"
-ls -la ../../node_modules/
-echo "openzeppelin dir:"
-ls -la ../../node_modules/@openzeppelin/
-echo "Legacy artifacts:"
-ls -la ../../node_modules/@openzeppelin/upgrades-core-legacy/artifacts/
-echo "pwd:"
-pwd
-echo "artifacts dir:"
-ls -la artifacts/
-
 cp -R ../../node_modules/@openzeppelin/upgrades-core-legacy/artifacts .
-
-echo "artifacts dir after:"
-ls -la artifacts/
